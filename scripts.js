030d76adc5ae583dbd6378814186d280aea863ed
Ali Aliyev <alyvofficial@gmail.com>
2024-12-11 01:46:53 +0400

    Implement dynamic quotes carousel with loading indicator and fetch quotes from API

--- scripts.js
+++ scripts.js
@@ -163,4 +163,38 @@ document.addEventListener("DOMContentLoaded", function () {
       carousel3.style.display = "block";
     })
     .catch((error) => console.error("Error fetching latest videos:", error));
+
+
+    // Fetch quotes for the first carousel
+  fetch("https://smileschool-api.hbtn.info/quotes")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((quote, index) => {
+      const activeClass = index === 0 ? "active" : "";
+      const carouselItem = `
+          <div class="carousel-item ${activeClass}">
+            <div class="row mx-auto align-items-center">
+              <div class="col-12 col-sm-2 col-lg-2 offset-lg-1 text-center">
+                <img src="${
+                  quote.pic_url
+                }" class="d-block align-self-center" alt="Carousel Pic ${
+        index + 1
+      }" />
+              </div>
+              <div class="col-12 col-sm-7 offset-sm-2 col-lg-9 offset-lg-0">
+                <div class="quote-text">
+                  <p class="text-white">« ${quote.text}</p>
+                  <h4 class="text-white font-weight-bold">${quote.name}</h4>
+                  <span class="text-white">${quote["sub-title"]}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        `;
+      carouselInner.insertAdjacentHTML("beforeend", carouselItem);
+    });
+    loader.style.display = "none";
+    carousel.style.display = "block";
+  })
+  .catch((error) => console.error("Error fetching quotes:", error));
 });
